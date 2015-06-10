0fe9a194eaca3ccf85b6384a759a3850a2df5574
mattdesl <dave.des@gmail.com>
2015-06-10 13:14:04 -0400

    bump to latest dependency

--- test/index.js
+++ test/index.js
@@ -31,8 +31,8 @@ lines.forEach(function (x) {
 function ready () {
   test('should have some asserts', function (t) {
     t.deepEqual(asserts[0], { name: 'got true', number: 1, ok: true, raw: 'ok 1 got true', test: 1, type: 'assert' })
-    t.deepEqual(asserts[1], { error: { actual: '51', at: { character: '5', file: 'basic.js', line: '5' }, expected: 'true', operator: 'equal', raw: { actual: '   51', at: ' Test.<anonymous> (basic.js:5:5)', expected: ' true', operator: ' equal' } }, name: 'something wrong', number: 2, ok: false, raw: 'not ok 2 something wrong', test: 1, type: 'assert' })
-    t.deepEqual(asserts[2], {  error: { actual: undefined, at: undefined, expected: undefined, operator: 'fail', raw: { operator: ' fail' } }, name: 'test timed out after 50ms', number: 3, ok: false, raw: 'not ok 3 test timed out after 50ms', test: 2, type: 'assert' })
+    t.deepEqual(asserts[1], { error: { actual: '51', at: { character: '5', file: 'basic.js', line: '5' }, expected: 'true', operator: 'equal', raw: '    operator: equal\n    expected: true\n    actual:   51\n    at: Test.<anonymous> (basic.js:5:5)' }, name: 'something wrong', number: 2, ok: false, raw: 'not ok 2 something wrong', test: 1, type: 'assert' })
+    t.deepEqual(asserts[2], { error: { actual: undefined, at: undefined, expected: undefined, operator: 'fail', raw: '    operator: fail' }, name: 'test timed out after 50ms', number: 3, ok: false, raw: 'not ok 3 test timed out after 50ms', test: 2, type: 'assert' })
     t.deepEqual(tests[0], { name: 'first test', number: 1, raw: '# first test', type: 'test' })
     t.deepEqual(result, { fail: 2, ok: false, pass: 1, total: 3 })
     t.end()
